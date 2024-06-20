4eb7f1d7a1e6d7b9252dc9d19abfe38fd31b479a
luchelin666 <luchelo03@gmail.com>
2024-06-19 22:37:24 -0500

    22:37

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { PrincipalComponent } from './components/principal/principal.component';
 import { LoginComponent } from './components/login/login.component';
 import { TestComponent } from './components/test/test.component';
+import { RegistrarEstudianteComponent } from './components/registrar-estudiante/registrar-estudiante.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/principal', pathMatch: 'full' }, //Indicamos que ni bien se ejecuta,se tenga la pagina1 de entrada
     { path: 'principal', component: PrincipalComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'test', component: TestComponent}
+    { path: 'test', component: TestComponent},
+    { path: 'registrar', component: RegistrarEstudianteComponent }
 ];
